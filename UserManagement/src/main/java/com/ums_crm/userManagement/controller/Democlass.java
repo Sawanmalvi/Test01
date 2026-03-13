2b9ffe07d9514ebe48252f1443f59c6a4f26358d
Sawanmalvi <sawanmalviya2468@gmail.com>
2026-03-13 23:24:42 +0530

    sawan_commit_jenkiens

--- UserManagement/src/main/java/com/ums_crm/userManagement/controller/Democlass.java
+++ UserManagement/src/main/java/com/ums_crm/userManagement/controller/Democlass.java
@@ -3,6 +3,7 @@ package com.ums_crm.userManagement.controller;
 public class Democlass {
     public static void main(String[] args) {
         System.out.println("demo run");
+        System.out.println("Run");
     }
 }
 
